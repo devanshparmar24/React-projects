a099aa3fce5595c98656ee8b28e733feab67719b
devanshparmar24 <devanshparmar13618@gmail.com>
2026-02-11 19:07:38 +0530

    -Applied Header and Footers -Applied Forward Reference for Input field -Different Button component

--- megaBlog/src/App.jsx
+++ megaBlog/src/App.jsx
@@ -1,15 +1,42 @@
+import { useDispatch } from 'react-redux';
+import React,{ useState,useEffect } from 'react';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const[loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
   
-  return (
-    <>
-      <div>
-        Mega Blog
-      </div>
-    
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}));
+      }else{
+        dispatch(logout());
+      }
+    })
+    .catch((error)=>{
+      console.error("Error fetching user data:", error);
+      dispatch(logout());
+    })
+    .finally(()=>{
+      setLoading(false);
+    })
+  }, [ dispatch ]);
+
+  return (!loading ? ( 
+    <div className="w-full h-screen flex flex-wrap content-between bg-gray-400">
+      <Header />
+      <main>
+        TODO: {/* <Outlet /> */}
+      </main>
+      <Footer />
+    </div> ) : ( <div className="loading">Loading...</div> )
+  );
 } 
 
 export default App
---
+++ megaBlog/src/components/index.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+import Footer from './Footer';
+import Logo from './Logo';
+import LogoutBtn from './Header/LogoutBtn';
+import Container from './container/Container';
+
+export {
+    Header,
+    Footer,
+    Logo,
+    LogoutBtn,
+    Container
+}
