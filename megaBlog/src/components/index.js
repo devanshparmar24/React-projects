597bdc0e1f447fa8e8df6ee45e30c994c9ce3127
devanshparmar24 <devanshparmar13618@gmail.com>
2026-02-13 00:25:46 +0530

    feat: add PostForm component for creating and updating posts

--- megaBlog/src/components/index.js
+++ megaBlog/src/components/index.js
@@ -3,11 +3,19 @@ import Footer from './Footer';
 import Logo from './Logo';
 import LogoutBtn from './Header/LogoutBtn';
 import Container from './container/Container';
+import Button from './Button';
+import Select from './Select';
+import Input from './Input';
+import RTE from './RTE';
 
 export {
     Header,
     Footer,
     Logo,
     LogoutBtn,
-    Container
+    Container,
+    Button,
+    Select,
+    Input,
+    RTE
 }
