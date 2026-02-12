451ada81c3f67a1404a7c24c1b323544d442959b
devanshparmar24 <devanshparmar13618@gmail.com>
2026-02-12 18:05:26 +0530

    feat: add Protected component for route authentication and loading state

--- megaBlog/src/store/store.js
+++ megaBlog/src/store/store.js
@@ -1,10 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
-import { login, logout } from './authSlice';
 
 const store = configureStore({
     reducer:{
-        login,
-        logout
+        
     }
 });
 
