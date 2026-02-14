a537964d3c03f73eaac5cfd2f33a8dfb8bb7ca9a
devanshparmar24 <devanshparmar13618@gmail.com>
2026-02-15 01:09:30 +0530

    feat: add pages for AddPost, AllPosts, EditPost, Home, Login, Post, and Signup with corresponding components

--- megaBlog/src/components/index.js
+++ megaBlog/src/components/index.js
@@ -7,6 +7,10 @@ import Button from './Button';
 import Select from './Select';
 import Input from './Input';
 import RTE from './RTE';
+import Signup from './Signup';
+import Login from './Login';
+import PostCard from './PostCard';
+import PostForm from './post-form/PostForm';
 
 export {
     Header,
@@ -17,5 +21,9 @@ export {
     Button,
     Select,
     Input,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    PostCard,
+    PostForm
 }
---
+++ megaBlog/src/pages/AllPosts.jsx
@@ -0,0 +1,30 @@
+import React, { use } from 'react'
+import appwriteService from '../appwrite/config'
+import { Container, PostCard } from '../components'
+
+
+function AllPosts() {
+    const [posts, setPosts] = React.useState([])
+    useEffect(() => {
+        appwriteService.getAllPosts([]).then((posts) => {
+            if (posts) {
+                setPosts(posts.documents)
+            }
+        })
+    }, [])
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard post={post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default AllPosts
